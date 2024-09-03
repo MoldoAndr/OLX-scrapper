086cbe442b7d574547256599739f09132af869fa
Andrei Moldovan <123833398+MoldoAndr@users.noreply.github.com>
2024-09-03 23:38:00 +0600

    .

--- OVOKO_scrapper.py
+++ OVOKO_scrapper.py
@@ -2,6 +2,8 @@ import re
 import time
 import random
 import urllib.parse
+import tkinter as tk
+from tkinter import messagebox
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -64,7 +66,7 @@ def setup_driver(brave_path):
     return driver
 
 def get_next_page_url(current_url, current_page):
-    return current_url + str(current_page) 
+    return current_url + str(current_page + 1) 
 
 def scrape_links(url, output_file, brave_path, start_page=1, end_page=1000):
     driver = setup_driver(brave_path)
@@ -127,11 +129,44 @@ def cut(file_path, keyword):
     except IOError as e:
         print(f"Error writing to the file {file_path}: {e}")
 
+def start_scraping():
+    url = url_entry.get()
+    brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
+    link_pattern = pattern_entry.get()
+    output_file = output_file_entry.get()
+    
+    
+    if not url or not link_pattern or not output_file or not brave_path:
+        messagebox.showerror("Error", "All fields are required.")
+        return
 
-url = "https://ovoko.pl/szukaj?man_id=76&cmc=1132&cm=2026&mfi=76,1132,2026;&prs=1&page="
-link_pattern = "/czesci-samochodowe/"
-all_links_file = "all_links.txt"
-brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+    try:
+        scrape_links(url, output_file, brave_path, start_page=1, end_page=int(number_URLS.get()))
+        cut(output_file, link_pattern)
+        messagebox.showinfo("Complete", "Scraping and processing complete!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
 
-scrape_links(url, all_links_file, brave_path, start_page=1, end_page=1000)
-cut(all_links_file, link_pattern)
+root = tk.Tk()
+root.title("Web Scraper")
+
+tk.Label(root, text="URL:").grid(row=0, column=0, padx=10, pady=5)
+url_entry = tk.Entry(root, width=50)
+url_entry.grid(row=0, column=1, padx=10, pady=5)
+
+tk.Label(root, text="Link Pattern:").grid(row=1, column=0, padx=10, pady=5)
+pattern_entry = tk.Entry(root, width=50)
+pattern_entry.grid(row=1, column=1, padx=10, pady=5)
+
+tk.Label(root, text="Output File:").grid(row=2, column=0, padx=10, pady=5)
+output_file_entry = tk.Entry(root, width=50)
+output_file_entry.grid(row=2, column=1, padx=10, pady=5)
+
+tk.Label(root, text="N.o. URL's:").grid(row=3, column=0, padx=10, pady=5)
+number_URLS = tk.Entry(root, width=50)
+number_URLS.grid(row=3, column=1, padx=10, pady=5)
+
+start_button = tk.Button(root, text="Start", command=start_scraping)
+start_button.grid(row=4, column=1, padx=10, pady=10)
+
+root.mainloop()
